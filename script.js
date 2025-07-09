b81b46b787e7a1ccd3354b42f5c611dd8d754488
Diego <iwujigoodluck5@gmail.com>
2025-07-09 17:22:34 +0000

    update

--- script.js
+++ script.js
@@ -2,18 +2,7 @@ const toggleDiv = document.querySelector("#toggleDiv");
 const viewLogs = document.querySelector("#viewLogs");
 
 toggleDiv.addEventListener("click", (e) => {
-  if (viewLogs.classList.contains("w-fit")) {
-    viewLogs.classList.remove("p-4");
-    viewLogs.classList.add("w-0");
-    viewLogs.classList.remove("w-fit");
-    viewLogs.classList.remove("inset-y-4");
-    viewLogs.classList.remove("top-4");
-  } else {
-    viewLogs.classList.add("p-4");
-    viewLogs.classList.remove("w-0");
-    viewLogs.classList.add("w-fit");
-    viewLogs.classList.add("inset-y-4");
-    viewLogs.classList.add("top-4");
-     
-  }
+  viewLogs.classList.toggle("p-4");
+  viewLogs.classList.toggle("w-0");
+  viewLogs.classList.toggle("w-fit");
 });
