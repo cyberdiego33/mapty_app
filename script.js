231298d89f68f5b1d0cf87920de78724eeb7f17d
Diego <iwujigoodluck5@gmail.com>
2025-07-09 16:41:23 +0000

    update

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ toggleDiv.addEventListener("click", (e) => {
     viewLogs.classList.remove("p-4");
     viewLogs.classList.add("w-0");
     viewLogs.classList.remove("w-fit");
+    viewLogs.classList.remove("inset-y-4");
+    viewLogs.classList.remove("top-4");
   } else {
     viewLogs.classList.add("p-4");
     viewLogs.classList.remove("w-0");
     viewLogs.classList.add("w-fit");
+    viewLogs.classList.add("inset-y-4");
+    viewLogs.classList.add("top-4");
+     
   }
 });
